7a3ae612d7751e07d2efca786d7235f71d607bb6
Atsushi Kokubu <akokubu@gmail.com>
2025-10-19 21:16:07 +0900

    log

--- main.ts
+++ main.ts
@@ -3,11 +3,19 @@ import { app } from "./app.ts";
 console.log("=== main.ts start ===");
 
 const port = 8080;
-console.log(`Setting up the server on localhost:${port}`);
+console.log(`Setting up the server on port ${port}`);
+
+setTimeout(() => {
+  console.log("=== Tick: 3秒経過 ===");
+}, 3000);
 
 try {
-  await app.listen({ port: port, hostname: "0.0.0.0" });
-  console.log("Server started successfully");
+  app
+    .listen({ port, hostname: "0.0.0.0" })
+    .then(() => console.log("Server started successfully"))
+    .catch((e) => console.error("Server failed to start:", e));
+
+  console.log("app.listen() called (non-blocking)");
 } catch (e) {
-  console.error("Server failed to start:", e);
+  console.error("Error calling app.listen:", e);
 }
