5cf529c76038c1c98caeb4f1f8af1176ce82e044
Atsushi Kokubu <akokubu@gmail.com>
2025-10-19 21:20:40 +0900

    port

--- main.ts
+++ main.ts
@@ -2,25 +2,16 @@ import { app } from "./app.ts";
 
 console.log("=== main.ts start ===");
 
-// const port = 8080;
-// console.log(`Setting up the server on port ${port}`);
-
-const port = Number(Deno.env.get("PORT")) || 8080;
+const port = Number(Deno.env.get("PORT")) || 8081;
 console.log(`Setting up the server on port ${port}`);
 
-await app.listen({ port, hostname: "0.0.0.0" });
+app
+  .listen({ port, hostname: "0.0.0.0" })
+  .then(() => console.log("Server started successfully"))
+  .catch((e) => console.error("Server failed to start:", e));
+
+console.log("app.listen() called (non-blocking)");
 
 setTimeout(() => {
   console.log("=== Tick: 3秒経過 ===");
 }, 3000);
-
-try {
-  app
-    .listen({ port, hostname: "0.0.0.0" })
-    .then(() => console.log("Server started successfully"))
-    .catch((e) => console.error("Server failed to start:", e));
-
-  console.log("app.listen() called (non-blocking)");
-} catch (e) {
-  console.error("Error calling app.listen:", e);
-}
