59ffb2c1d39d6369d4472dfb8093e4cc773e6e61
Atsushi Kokubu <akokubu@gmail.com>
2025-10-19 18:30:55 +0900

    host, port

--- main.ts
+++ main.ts
@@ -3,4 +3,4 @@ import { app } from "./app.ts";
 const port = 8080;
 console.log(`Setting up the server on localhost:${port}`);
 
-await app.listen({ port });
+await app.listen({ port: port, hostname: "0.0.0.0" });
